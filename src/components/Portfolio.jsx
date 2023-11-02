83cc8d60230d304a5612ecbff1c1706fcc7a8b39
Kervin Gonzalez <kervingonzalez@gmail.com>
2023-11-02 15:58:51 -0400

    ajustes

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,24 +1,24 @@
-import { useContext } from 'react'
-import { UserContext } from '../App';
+import { useContext } from "react";
+import { UserContext } from "../App";
 
 export default function Portfolio() {
-    const data = useContext(UserContext);
-    const {title, portfolios} = data.portfolio
+  const data = useContext(UserContext);
+  const { title, portfolios } = data.portfolio;
   return (
     <div className="w-full min-h-max lg:px-24 px-4">
-        <h1 className="title text-yellow-600">
-            {title}
-      </h1>
-      <div className='flex gap-2'>
-        {portfolios.map((p, i)=>(
+      <h1 className="title text-yellow-600">{title}</h1>
+      <div className="flex gap-2">
+        {portfolios.map((p, i) => (
           <div key={i}>
-            <img src={`../assets/portfolio/${p.image}.png`}/>
-            <p className='uppercase mt-2'>
-               <a target="_blank" rel="noopener noreferrer" href={p.url}>{p.name}</a>
+            <img src={`/assets/portfolio/${p.image}`} />
+            <p className="uppercase mt-2">
+              <a target="_blank" rel="noopener noreferrer" href={p.url}>
+                {p.name}
+              </a>
             </p>
           </div>
         ))}
       </div>
     </div>
-  )
+  );
 }
